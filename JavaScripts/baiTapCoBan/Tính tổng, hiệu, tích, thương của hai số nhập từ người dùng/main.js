7314d84f7a4a1a89484241903a2c738293a6321e
Nguyen-Thanh-Tung04 <thanhtung2432004@gmail.com>
2024-09-08 22:04:55 +0700

    bài tập javascript

---
+++ JavaScripts/baiTapCoBan/Tính tổng, hiệu, tích, thương của hai số nhập từ người dùng/main.js
@@ -0,0 +1,12 @@
+let num1 = parseFloat(prompt("Nhập số thứ nhất:"));
+let num2 = parseFloat(prompt("Nhập số thứ hai:"));
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Tổng: " + sum);
+console.log("Hiệu: " + difference);
+console.log("Tích: " + product);
+console.log("Thương: " + quotient);
